0275e016c77a3e575a14dc862861d7dc0cf6d71c
RealSpeaky <realspeaky@gmail.com>
2025-05-16 16:35:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ async def ping(ctx):
     await ctx.send('Pong!')
 
 # Run the bot (use your actual bot token here)
-bot.run("MTM3MjU0OTQ3MTg1NDAwNjMxMw.GzxP_G._iPFrExBkBt6EdzLAsw58pVudf-cv99jApyFPs")
+bot.run("MTM3MjU0OTQ3MTg1NDAwNjMxMw.G7sTOe.FzXnk7GPmKsSiHlC6IrjHHYSitLhIBPsXPzF5g")
